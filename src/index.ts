e175d2a1a43a65e877bd11f54de9fafd738a6dfd
Artem Tarasenko <shabashab.04@gmail.com>
2022-04-12 20:36:09 +0300

    refactor: index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import * as path from "path";
 import { FtpAsyncClient } from "./ftp-async-client";
 import { listGlobFiles } from "./glob.helper";
 
+type LogFunction = (str: string) => void;
+
+interface DeployContext {
+  options: DeployOptions;
+  ftpClient: FtpAsyncClient;
+  logFunction: LogFunction;
+}
+
 export interface DeployConnectionOptions {
   host: string;
   user: string;
@@ -22,8 +30,64 @@ export interface DeployOptions {
   connection: DeployConnectionOptions;
   replace: boolean;
 
-  logger?: (str: string) => void;
+  logger?: LogFunction;
 }
+
+const prepareDeployDirectory = async ({ftpClient, logFunction, options}:DeployContext): Promise<void> => {
+  const directoryExists = await ftpClient.dirExists(options.remotePath);
+
+  if(!directoryExists) {
+    logFunction(`Remote directory ${options.remotePath} does not exist. Creating a directory`);
+    await ftpClient.mkdir(options.remotePath, true);
+
+  } else if(options.cleanRemoteDirectory) {
+    const listedEntries = await ftpClient.list(options.remotePath);
+
+    logFunction(`Found ${listedEntries.length} entries in remote directory. Deleting...`)
+
+    const lastWorkingDirectory = await ftpClient.pwd();
+    await ftpClient.cwd(options.remotePath);
+
+    for(const entry of listedEntries) {
+      if(options.excludeFromCleaning?.some(value => value === entry.name)) {
+        logFunction(`Entry ${entry.name} is excluded. Skipping...`);
+        continue;
+      }
+
+      if(entry.type == "d") {
+        await ftpClient.rmdir(entry.name, true);
+        logFunction(`Remote directory ${path.join(options.remotePath, entry.name)} has been deleted`)
+      } else {
+        await ftpClient.rm(entry.name);
+        logFunction(`Remote file ${path.join(options.remotePath, entry.name)} has been deleted`)
+      }
+    }
+
+    await ftpClient.cwd(lastWorkingDirectory);
+  }
+};
+
+const transferFiles = async (files: string[], {
+  ftpClient, logFunction, options
+}: DeployContext): Promise<void> => {
+  for(const file of files) {
+    const sourcePath = path.resolve(process.cwd(), options.sourcePath, file);
+    const destinationPath = path.join(options.remotePath, file);
+
+    logFunction(`Transfering ${file} to ${destinationPath}`);
+
+    if(!options.replace && await ftpClient.fileExists(destinationPath)) {
+      logFunction(`${destinationPath} already exists. skipping...\n`)
+      continue;
+    }
+
+    const destinationDirname = path.dirname(destinationPath);
+    await ftpClient.mkdir(destinationDirname, true);
+
+    await ftpClient.putFile(sourcePath, destinationPath);
+    logFunction(`${file} has been successfully transfered\n`);
+  }
+};
 
 export const deploy = async (options: DeployOptions): Promise<void> => {
   const log = options.logger || (() => {});
@@ -50,61 +114,21 @@ export const deploy = async (options: DeployOptions): Promise<void> => {
     port: options.connection.port,
   });
 
+  const context: DeployContext = {
+    ftpClient: ftpClient,
+    logFunction: log,
+    options: options
+  };
+
   log(`Connected to ${options.connection.host} as ${options.connection.user}\n`);
 
-  const directoryExists = await ftpClient.dirExists(options.remotePath);
-
-  if(!directoryExists) {
-    log(`Remote directory ${options.remotePath} does not exist. Creating a directory`);
-    await ftpClient.mkdir(options.remotePath, true);
-
-  } else if(options.cleanRemoteDirectory) {
-    const listedEntries = await ftpClient.list(options.remotePath);
-
-    log(`Found ${listedEntries.length} entries in remote directory. Deleting...`)
-
-    const lastWorkingDirectory = await ftpClient.pwd();
-    await ftpClient.cwd(options.remotePath);
-
-    for(const entry of listedEntries) {
-      if(options.excludeFromCleaning?.some(value => value === entry.name)) {
-        log(`Entry ${entry.name} is excluded. Skipping...`);
-        continue;
-      }
-
-      if(entry.type == "d") {
-        await ftpClient.rmdir(entry.name, true);
-        log(`Remote directory ${path.join(options.remotePath, entry.name)} has been deleted`)
-      } else {
-        await ftpClient.rm(entry.name);
-        log(`Remote file ${path.join(options.remotePath, entry.name)} has been deleted`)
-      }
-    }
-
-    await ftpClient.cwd(lastWorkingDirectory);
-  }
+  await prepareDeployDirectory(context);
 
   log("");
 
   log("Starting file transfer...\n");
 
-  for(const file of files) {
-    const sourcePath = path.resolve(process.cwd(), options.sourcePath, file);
-    const destinationPath = path.join(options.remotePath, file);
-
-    log(`Transfering ${file} to ${destinationPath}`);
-
-    if(!options.replace && await ftpClient.fileExists(destinationPath)) {
-      log(`${destinationPath} already exists. skipping...\n`)
-      continue;
-    }
-
-    const destinationDirname = path.dirname(destinationPath);
-    await ftpClient.mkdir(destinationDirname, true);
-
-    await ftpClient.putFile(sourcePath, destinationPath);
-    log(`${file} has been successfully transfered\n`);
-  }
+  await transferFiles(files, context);
 
   log("File transfer has been successfully finished!\n")
   log("Successful deploy! Disconnecting...")
